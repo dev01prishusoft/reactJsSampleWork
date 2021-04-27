e5e19967a3f0033833831b27695f16a6be3841c9
Hiren <Hiren>
2021-04-27 11:02:14 +0530

    started project

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# reactJsSampleWork
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TaskWrapper from './task/task';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskWrapper />
+    </div>
+  );
+}
+
+export default App;
